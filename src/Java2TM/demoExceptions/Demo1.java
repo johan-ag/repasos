14fdf81474cf9e58525a953850a3eacb613ae8f6
Johan Avila Guerrero <johan.avila@mercadolibre.com.mx>
2021-08-07 19:44:17 -0500

    Java 2 Module ended

--- src/Java2TM/demoExceptions/Demo1.java
+++ src/Java2TM/demoExceptions/Demo1.java
@@ -1,5 +1,25 @@
 package Java2TM.demoExceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Demo1 {
+
+    public static void main(String[] args) {
+        try {
+            FileInputStream file = new FileInputStream("./miarchivo.txt");
+
+            int c = file.read();
+
+            while (c>=0){
+                System.out.print((char)c);
+                c=file.read();
+            }
+
+            file.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
